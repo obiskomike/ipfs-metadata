7ab82e73f207a21f248243e97780af20294b1c52
haidermunir810 <haidermunir810@gmail.com>
2024-11-20 14:36:44 +0500

    add terraform

---
+++ terraform/alb.tf
@@ -0,0 +1,24 @@
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev"
+  load_balancer_type = "application"
+  subnets            = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+  security_groups    = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
---
+++ terraform/ecs.tf
@@ -0,0 +1,49 @@
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "aipfs-metadata-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-first-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-first-service"
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.app_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    container_name   = "${aws_ecs_task_definition.app_task.family}"
+    container_port   = 8080
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
+  }
+}
